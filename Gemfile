54365654aa6105cbbf5ee0b7ed3a547eb90eeac6
Zia123456 <m.ziabakhteyari12@gmail.com>
2023-02-25 03:01:16 -0800

    Add linters

--- Gemfile
+++ Gemfile
@@ -70,3 +70,4 @@ group :test do
   gem "selenium-webdriver"
   gem "webdrivers"
 end
+gem 'rubocop', '>= 1.0', '< 2.0'
---
+++ db/migrate/20230225100204_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :users do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :photo
+      t.string :bio
+      t.integer :post_counter
+
+      t.timestamps
+    end
+  end
+end
