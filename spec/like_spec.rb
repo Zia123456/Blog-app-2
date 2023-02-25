35636960cc393cf2764992ae840cca32c22296c5
Zia123456 <m.ziabakhteyari12@gmail.com>
2023-02-25 10:34:11 -0800

    Fix error

--- spec/like_spec.rb
+++ spec/like_spec.rb
@@ -1,7 +1,8 @@
 require_relative './rails_helper'
 
 RSpec.describe Like, type: :model do
-  user = User.create(name: 'John', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico', posts_counter: 0)
+  user = User.create(name: 'John', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico',
+                     posts_counter: 0)
   post = Post.create(title: 'Bio', text: 'Teacher from Poland', comments_counter: 0, likes_counter: 0, author: user)
   subject { Like.new(author: user, post:) }
 
