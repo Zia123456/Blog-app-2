36995d59dc8188aa6d09617d24f83247572bf84d
Zia123456 <m.ziabakhteyari12@gmail.com>
2023-02-25 03:36:15 -0800

    Fix error

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -1,4 +1,3 @@
 class BlogController < ApplicationController
-  def index
-  end
+  def index; end
 end
--- test/controllers/blog_controller_test.rb
+++ test/controllers/blog_controller_test.rb
@@ -1,4 +1,4 @@
-require "test_helper"
+require 'test_helper'
 
 class BlogControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
