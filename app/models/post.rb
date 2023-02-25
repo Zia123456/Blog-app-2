466d351cc6cd0e74bbb956e2b6530174d9e39b13
Zia123456 <m.ziabakhteyari12@gmail.com>
2023-02-25 10:26:54 -0800

    Add validation, create rspec unit-test

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,6 +9,10 @@ class Post < ApplicationRecord
     comments.order(created_at: :desc).limit(5)
   end
 
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :comments_counter, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likes_counter, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
   private
 
   def update_posts_counter
---
+++ spec/comment_spec.rb
@@ -0,0 +1,23 @@
+require_relative './rails_helper'
+
+RSpec.describe Comment, type: :model do
+  user = User.create(name: 'John', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico', posts_counter: 0)
+  post = Post.create(title: 'Bio', text: 'Teacher from Poland', comments_counter: 0, likes_counter: 0, author: user)
+  subject { Comment.new(text: 'Hi', author: user, post:) }
+
+  before { subject.save }
+
+  it 'comments counter must be 1' do
+    expect(post.comments_counter).to eq 1
+  end
+
+  it 'error when user is empty' do
+    subject.author = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'error when post is empty' do
+    subject.post = nil
+    expect(subject).to_not be_valid
+  end
+end
