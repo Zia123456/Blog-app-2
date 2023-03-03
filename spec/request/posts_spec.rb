adf34d5009e6f4ab4906254b95e937e77f22ce08
Zia123456 <m.ziabakhteyari12@gmail.com>
2023-03-03 07:27:17 -0800

    Debug

--- spec/request/posts_spec.rb
+++ spec/request/posts_spec.rb
@@ -2,7 +2,8 @@ require 'rails_helper'
 
 RSpec.describe 'Controllers', type: :request do
   describe 'GET #index' do
-    before(:example) { get users_path } # get(:index)
+  let(:user) { User.create(name: 'Zia', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'I am a teacher from maxico', posts_counter: 1) }
+    before(:example) { get user_posts_path(user) } # get(:index)
     it 'is a success' do
       expect(response).to have_http_status(:ok)
     end
@@ -12,12 +13,15 @@ RSpec.describe 'Controllers', type: :request do
   end
 
   describe 'GET #show' do
-    before(:example) { get users_path } # get(:show)
+    let(:user) { User.create(name: 'Zia', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'I am a teacher from maxico', posts_counter: 1) }
+    let(:post) {Post.create(title: 'software', text: 'I love software engineering', comments_counter: 1, likes_counter: 1, author: user) }
+   
+    before(:example) { get user_post_path(user, post) } # get(:show)
     it 'is a success' do
       expect(response).to have_http_status(:ok)
     end
     it "renders 'index' template" do
-      expect(response).to render_template(:index)
+      expect(response).to render_template(:show)
     end
   end
 end
--- spec/request/users_spec.rb
+++ spec/request/users_spec.rb
@@ -9,15 +9,22 @@ RSpec.describe 'Controllers', type: :request do
     it "renders 'index' template" do
       expect(response).to render_template('index')
     end
+    it "show index content" do
+      expect(response.body).to include('Here is a list of users')
+    end
   end
 
   describe 'GET #show' do
-    before(:example) { get users_path } # get(:show)
+  let(:user) { User.create(name: 'Zia', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'I am a teacher from maxico', posts_counter: 1) }
+    before(:example) { get user_path(user) } # get(:show)
     it 'is a success' do
       expect(response).to have_http_status(:ok)
     end
     it "renders 'index' template" do
-      expect(response).to render_template(:index)
+      expect(response).to render_template(:show)
+    end
+    it "shows show content" do
+      expect(response.body).to include('Here is details of a single user')
     end
   end
 end
