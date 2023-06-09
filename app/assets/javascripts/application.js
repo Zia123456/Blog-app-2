e63ee1c76140cc1229cadebd1019ac52ea5f4e0f
Zia123456 <m.ziabakhteyari12@gmail.com>
2023-06-09 10:01:18 -0700

    Add js for search-box

--- app/assets/javascripts/application.js
+++
@@ -1,5 +0,0 @@
-//= require rails-ujs
-//= require activestorage
-//= require_tree .
-//= require search
-import { performSearch } from './search';
--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,28 +1,10 @@
 // app/assets/javascripts/search.js
+function performSearch() {
+  const navDiv = document.querySelector('.nav');
 
-export function performSearch() {
-  document.addEventListener('DOMContentLoaded', function () {
-    const searchInput = document.getElementById('search-query');
-    const searchResults = document.getElementById('search-results');
-
-    searchInput.addEventListener('input', function () {
-      const query = searchInput.value;
-
-      if (query.length >= 3) {
-        searchResults.innerHTML = ''; // Clear previous results
-
-        fetch('/search?query=' + query)
-          .then((response) => response.json())
-          .then((users) => {
-            users.forEach((user) => {
-              const userElement = document.createElement('p');
-              userElement.textContent = user.name;
-              searchResults.appendChild(userElement);
-            });
-          });
-      } else {
-        searchResults.innerHTML = ''; // Clear results if query length is less than 3
-      }
-    });
-  });
+  // Manipulate the selected element
+  navDiv.style.backgroundColor = 'blue';
+  console.log(navDiv);
 }
+
+performSearch();
